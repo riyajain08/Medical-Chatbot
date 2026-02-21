3d57bedd43b44e42d3bccb9b1d2a23a124746b40
Riya <jainrhea2004@gmail.com>
2026-02-21 23:19:04 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup (
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Riya",
+    author_email="goyalriya6682@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
